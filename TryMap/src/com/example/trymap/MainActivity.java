29881d2e9a61005b9ccab47681858c161b2d4602
sehrose <seh007bond@gmail.com>
2013-07-05 23:42:59 +0400

    ok

--- TryMap/src/com/example/trymap/MainActivity.java
+++ TryMap/src/com/example/trymap/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.trymap;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		setContentView(R.layout.activity_splash);
+		
+		Thread logotimer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+					Intent mainIntent = new Intent("com.example.trymap.SplashActivity");
+					startActivity(mainIntent);
+					
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				finally{
+					finish();
+					
+				}
+			}
+		};
+		
+		logotimer.start();
 	}
 
 }
